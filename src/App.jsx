576aa72efaa6a4a51d430d1e4c92e9e6377bcaec
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-11-30 17:50:05 +1300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Globalstyles, {css} from 'styled-components'
+
+const App = () => {
+	return (
+		<div>
+			<h1>App</h1>
+		</div>
+	)
+}
+
+export default App
