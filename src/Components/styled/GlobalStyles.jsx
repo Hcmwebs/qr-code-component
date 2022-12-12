43f1f608384e738e1705dde030c99c0e7d86f896
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-12-13 09:34:45 +1300

    Removed the font import and added several resets

--- src/Components/styled/GlobalStyles.jsx
+++ src/Components/styled/GlobalStyles.jsx
@@ -1,24 +1,24 @@
-
-import { createGlobalStyle} from 'styled-components';
+import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
 
-@import url('https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&display=swap');
-*{
+*, *::before, *::after{
   padding: 0;
 	margin: 0;
-	box-sizing:border-box
+	box-sizing:border-box;
+  font: inherit;
 }
 
 html{
-	font-size:100%
+	font-size:100%;
+  color-scheme:dark light;
 }
 
 body {
 	min-height:100vh;
 	font-family: 'Outfit', sans-serif;
   line-height: 1.75;
-  line-height:400;
+  font-weight:400;
 	background: rgba(213, 225, 239, 1);
 	display: grid;
 	place-items: center;
@@ -35,7 +35,7 @@ h1, h2, h3, h4, h5 {
 }
 
 h1 {
-  margin-top: 0;
+
   font-size: 3.052rem;
 }
 
@@ -49,6 +49,12 @@ h5 {font-size: 1.25rem;}
 
 small, .text_small {font-size: 0.8rem;}
 
+
+
+img, picture, svg, video {
+  display: block;
+  max-width:100%;
+}
 `
 
-export default GlobalStyles;
+export default GlobalStyles
