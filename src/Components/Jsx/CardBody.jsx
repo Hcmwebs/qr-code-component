be13c94cbdd1261a8a68d22cd136164b75f68268
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-12-13 08:02:07 +1300

    completed moved the CardBody component

--- src/Components/Jsx/CardBody.jsx
+++ src/Components/Jsx/CardBody.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { StyledCardBody } from '../Styled'
 
 const CardBody = () => {
 	return (
 		<>
-			<h3>This a card body component</h3>
+			<StyledCardBody>
+				<h1>Improve your front-end skills by building projects</h1>
+				<p>
+					Scan the QR code to visit Frontend Mentor and take your coding skills
+					to the next level
+				</p>
+			</StyledCardBody>
 		</>
 	)
 }
