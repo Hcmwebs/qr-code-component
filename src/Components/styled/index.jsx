bb1f65540ea3a274a43de8f048b038bb9ca84ed3
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-12-12 14:39:49 +1300

    Individualized the card header and body and imported them accordingly

--- src/Components/styled/index.jsx
+++ src/Components/styled/index.jsx
@@ -1,6 +1,8 @@
 import GlobalStyles from './GlobalStyles'
 import StyledContainer from './StyledContainer'
 import StyledCard from './StyledCard'
+import StyledCardHeader from './StyledCardHeader'
+import StyledCardBody from './StyledCardBody'
 
 
-export { GlobalStyles, StyledContainer, StyledCard}
+export { GlobalStyles, StyledContainer, StyledCard, StyledCardHeader, StyledCardBody }
